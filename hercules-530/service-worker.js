a3f3854e0cbf56310872e3a15dea2ca371b50d80
mdan5678-dotcom <mdan5678@aol.com>
2026-08-01 21:55:07 -0500

    Cache complete Hercules 5:30 app for offline use

--- hercules-530/service-worker.js
+++ hercules-530/service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='hercules-530-v1.0.0';
-const CORE=['./','./index.html','./styles.css','./data.js','./app.js','./manifest.webmanifest','./icon.svg','./hercules-530-reminders.ics'];
+const CACHE='hercules-530-v1.0.1';
+const CORE=['./','./index.html','./styles.css','./data.js','./app.js','./app.part1.txt','./app.part2.txt','./app.part3.txt','./manifest.webmanifest','./icon.svg','./hercules-530-reminders.ics'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
