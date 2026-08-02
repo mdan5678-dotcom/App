c4c002e12863bc259526e6952df60285e074422c
mdan5678-dotcom <mdan5678@aol.com>
2026-08-01 21:51:36 -0500

    Add Hercules 5:30 offline service worker

---
+++ hercules-530/service-worker.js
@@ -0,0 +1,10 @@
+const CACHE='hercules-530-v1.0.0';
+const CORE=['./','./index.html','./styles.css','./data.js','./app.js','./manifest.webmanifest','./icon.svg','./hercules-530-reminders.ics'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>{
+  if(e.request.method!=='GET') return;
+  e.respondWith(caches.match(e.request).then(hit=>hit||fetch(e.request).then(r=>{
+    const copy=r.clone(); caches.open(CACHE).then(c=>c.put(e.request,copy)); return r;
+  }).catch(()=>caches.match('./index.html'))));
+});
